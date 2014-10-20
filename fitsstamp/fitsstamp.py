2a79d5cd30207ad0ec5e97028f224274099e547d
Matt Mechtley <mmechtley@gmail.com>
2014-10-20 17:36:45 +0200

    Generalized slicing for >2 dimensions (support label images for multi-dimensional datacubes). Only re-center celestial WCS when WCS is present (and only for 2 dimensions for now). Docstring and readability improvements

--- fitsstamp/fitsstamp.py
+++ fitsstamp/fitsstamp.py
@@ -2,15 +2,41 @@
 Utility methods for cutting image stamps from fits files. Various methods for
 specifying which region to cut.
 """
+from __future__ import division
 import astropy.io.fits as fits
 import astropy.wcs as wcs
 import pyregion
+import warnings
 from numpy import where, isfinite, array
 import re
 
 _stamp_slice_key = 'STAMPSLC'
 _stamp_slice_comment = 'Numpy array slice [Y,X] of stamp area in original image'
-_stamp_slice_format = '[{0:d}:{1:d},{2:d}:{3:d}]'
+
+
+def _slice_to_string(nd_slice):
+    """
+    Convert an n-dimensional numpy slice into a string. Use only start and stop
+    (step not used).
+    :param nd_slice: Sequence of slice objects (e.g. for numpy indexing)
+    :rtype: String representation of slice: [a:b, ... , y:z]
+    """
+    slice_str = ['{0:d}:{1:d}'.format(nd_slice[dim].start, nd_slice[dim].stop)
+                 for dim in xrange(len(nd_slice))]
+    return '[' + ','.join(slice_str) + ']'
+
+
+def _string_to_slice(slice_str):
+    """
+    Convert string specifying a numpy n-dimensional slice into
+    :param slice_str: String in usual numpy format: [a:b, ... , y:z]
+    :rtype: Tuple of slices represented in slice_string
+    """
+    slice_str = slice_str.strip('[]')
+    slice_str = re.split('\D+', slice_str)
+    slice_str = [int(num) for num in slice_str]
+    return tuple([slice(slice_str[2*dim], slice_str[2*dim + 1])
+                  for dim in xrange(len(slice_str) // 2)])
 
 
 def _stamp_from_regions(hdu_list, regions, extension=0, **kwargs):
@@ -60,8 +86,9 @@ def _stamp_from_mask(hdu_list, mask, extension=0, masked_value=None,
     :rtype: FITS PrimaryHDU with the original image header information.
     """
     masked_indexes = where(mask)
-    extents = (slice(masked_indexes[0].min(), masked_indexes[0].max()+1),
-               slice(masked_indexes[1].min(), masked_indexes[1].max()+1))
+    extents = tuple([slice(masked_indexes[dim].min(),
+                           masked_indexes[dim].max()+1)
+                     for dim in xrange(len(masked_indexes))])
     mask = mask[extents]
 
     # copy the data of the hduList
@@ -73,21 +100,32 @@ def _stamp_from_mask(hdu_list, mask, extension=0, masked_value=None,
         new_data[~mask] = masked_value
     new_hdu = fits.PrimaryHDU(header=hdu_list[extension].header, data=new_data)
 
-    # Re-Center WCS
     wcs_file = wcs.WCS(hdu_list[extension].header)
-    # Center is average of min and max pixels. Fits standard is 1-based and X,Y.
-    # Extents slice is numpy 0-based, stop non-inclusive, and Y,X
-    center = array((0.5*(extents[1].start + 1 + extents[1].stop),
-                    0.5*(extents[0].start + 1 + extents[0].stop)))
-    # The second argument here specifies the coordinate origin (1 for FITS)
-    wcs_file.wcs.crval = wcs_file.all_pix2world([center], 1)[0]
-    # Center in new image. Equivalently, 0.5*(reversed(data.shape)+1)
-    wcs_file.wcs.crpix = center - (extents[1].start, extents[0].start)
-    new_hdu.header.extend(wcs_file.to_header(), update=True)
+    coord_types = [axis['coordinate_type'] for axis
+                   in wcs_file.get_axis_types()]
+    if all([ctype == 'celestial' for ctype in coord_types]):
+        # Re-Center WCS if celestial coordinate transform is present
+        # Center is average of min and max pixels. Fits standard is 1-based and
+        # X,Y. Extents slice is numpy 0-based, stop non-inclusive, and Y,X
+        center = array((0.5*(extents[1].start + 1 + extents[1].stop),
+                        0.5*(extents[0].start + 1 + extents[0].stop)))
+        # The second argument here specifies the coordinate origin (1 for FITS)
+        wcs_file.wcs.crval = wcs_file.all_pix2world([center], 1)[0]
+        # Center in new image. Equivalently, 0.5*(reversed(data.shape)+1)
+        wcs_file.wcs.crpix = center - (extents[1].start, extents[0].start)
+        new_hdu.header.extend(wcs_file.to_header(), update=True)
+    elif all([ctype is None for ctype in coord_types]):
+        # When no WCS information is present, skip coordinate re-centering
+        # without comment to the user.
+        pass
+    else:
+        warnings.warn('File {} contains a mixture of celestial and non-'
+                      'celestial WCS axes. No WCS re-centering will be '
+                      'performed.'.format(hdu_list.filename()),
+                      wcs.AstropyUserWarning)
     new_hdu.update_header()
 
-    orig_sec_str = _stamp_slice_format.format(
-        extents[0].start, extents[0].stop, extents[1].start, extents[1].stop)
+    orig_sec_str = _slice_to_string(extents)
     new_hdu.header[_stamp_slice_key] = (orig_sec_str, _stamp_slice_comment)
     return new_hdu
 
@@ -167,19 +205,20 @@ def cut_stamps(source_file, region_file, region_format='region', source_ext=0,
         create composite, multi-component regions.
     "label": Specifies regfile is a label image where each object's pixels are
         flagged by a unique non-zero integer (e.g., sextractor segmentation map)
+        Datacubes with more than 2 dimensions are supported for label images.
 
     :param source_file: Filename of the image to cut from
     :param region_file: Filename specifying the regions to cut. Expected format
         is controlled by the region_format argument.
     :param region_format: The file format of regfile. Current options:
-        "region" - for SAOImage DS9 region files
-        "label" - for FITS images with integer unique labels for each object
-        (i.e. sextractor segmentation maps)
+        "region", "ds9" - for SAOImage DS9 region files
+        "label", "segmentation", "sextractor" - for FITS images with integer
+        unique labels for each object (i.e. sextractor segmentation maps)
     :param source_ext: FITS extension that contains the image data to cut
-    :param file_prefix: Base filename, region name or number is appended.
-        E.g., if cutting from weight maps, perhaps "wht_"
+    :param file_prefix: Base filename, to which region name or number is
+        appended. E.g., if cutting from weight maps, perhaps "wht_"
     :param selected_labels: List of specific label numbers (or DS9 region text
-        attributes) to output. Otherwise stamps are cut for all labels/regions.
+        attributes) to output. If None, stamps are cut for all labels/regions.
     :param masked_value: Value with which to fill masked-out regions of stamps.
         E.g. when cutting weight map stamps, masked_value=0 may be useful.
     :param mask_inf_nan: Whether to replace inf/nan pixels with masked_value
@@ -204,19 +243,20 @@ def paste_stamps(target_file, stamp_files, target_ext=0, stamps_ext=0,
                  output_name=None):
     """
     Pastes a collection of cut stamp images back into an original image. For
-    instance, if you cut a stamp, do a point source subtraction on a quasar, and
-    want to propagate the modified stamp back into the original image.
+    instance, if you cut a stamp, model out a galaxy or star using e.g. galfit,
+    and want to propagate the modified stamp back into the original image.
 
     :param target_file: Image that the stamps will be pasted into
     :param stamp_files: Stamp images that will be pasted into dest_file. Can
-        either be a single filename or a list of filenames.
+        either be a single filename or a list of filenames. Must retain the
+        STAMPSLC FITS keyword originally added during cutting.
     :param target_ext: FITS extension of dest_file to paste into
     :param stamps_ext: FITS extension of the stamp images to copy from
-    :param output_name: Name of the new file with pasted stamps. If none is
-        specified, a "_pasted" suffix will be appended to dest_file. E.g.
-        original.fits becomes original_pasted.fits. If output_name is the same
-        as dest_file, dest_file will be overwritten.
-    :rtype: Name of the modified target_file (i.e. value of output_name)
+    :param output_name: Name of the new file with pasted stamps. If None,
+        the suffix "_pasted" will be appended to dest_file. E.g. original.fits
+        becomes original_pasted.fits. If output_name is the same as dest_file,
+        dest_file will be overwritten.
+    :rtype: Filename of the modified copy of target_file
     """
     if isinstance(stamp_files, basestring):
         stamp_files = [stamp_files]
@@ -225,11 +265,8 @@ def paste_stamps(target_file, stamp_files, target_ext=0, stamps_ext=0,
     destination = fits.open(target_file)
     for stamp_file in stamp_files:
         stamp = fits.open(stamp_file)
-        paste_slice = stamp[stamps_ext].header[_stamp_slice_key].strip('[]')
-        paste_slice = re.split('\D+', paste_slice)
-        paste_slice = [int(num) for num in paste_slice]
-        paste_slice = (slice(paste_slice[0], paste_slice[1]),
-                       slice(paste_slice[2], paste_slice[3]))
+        paste_slice = stamp[stamps_ext].header[_stamp_slice_key]
+        paste_slice = _string_to_slice(paste_slice)
         slice_size = tuple([sl.stop - sl.start for sl in paste_slice])
         if slice_size != stamp[stamps_ext].data.shape:
             raise ValueError('Shape of stamp image does not match shape of '
